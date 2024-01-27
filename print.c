e80c6fee475748aba7ac09690b9f44d68f5f8bd1
Sejin Kim <sejkim2@c2r9s2.42seoul.kr>
2024-01-27 12:05:43 +0900

    [add : file extension check, test_print file move to print.c]

---
+++ print.c
@@ -0,0 +1,8 @@
+// printf("A : [%f] [%f %f %f]\n", mrt->amb.ratio, mrt->amb.color.x, mrt->amb.color.y, mrt->amb.color.z);
+	// printf("C : [%f %f %f] [%f %f %f] [%f]\n", mrt->cam.eye.x, mrt->cam.eye.y, mrt->cam.eye.z, mrt->cam.at.x, mrt->cam.at.y, mrt->cam.at.z, mrt->cam.fov);
+	// printf("L : [%f %f %f] [%f] [%f %f %f]\n", mrt->lit.origin.x, mrt->lit.origin.y, mrt->lit.origin.z, mrt->lit.bright, mrt->lit.color.x, mrt->lit.color.y, mrt->lit.color.z);
+	// printf("sp : [%f %f %f] [%f] [%f %f %f]\n", mrt->objs->origin.x, mrt->objs->origin.y, mrt->objs->origin.z, mrt->objs->radius, mrt->objs->albedo.x, mrt->objs->albedo.y, mrt->objs->albedo.z);
+	// mrt->objs = mrt->objs->next;
+	// printf("pl : [%f %f %f] [%f %f %f] [%f %f %f]\n", mrt->objs->origin.x, mrt->objs->origin.y, mrt->objs->origin.z, mrt->objs->axis.x, mrt->objs->axis.y, mrt->objs->axis.z, mrt->objs->albedo.x, mrt->objs->albedo.y, mrt->objs->albedo.z);
+	// mrt->objs = mrt->objs->next;
+	// printf("cy : [%f %f %f] [%f %f %f] [%f] [%f] [%f %f %f]\n", mrt->objs->origin.x, mrt->objs->origin.y, mrt->objs->origin.z, mrt->objs->axis.x, mrt->objs->axis.y, mrt->objs->axis.z, mrt->objs->radius, mrt->objs->height, mrt->objs->albedo.x, mrt->objs->albedo.y, mrt->objs->albedo.z);
--- sources/mrt/mrt_init.c
+++ sources/mrt/mrt_init.c
@@ -6,7 +6,7 @@
 /*   By: sejkim2 <sejkim2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 23:39:41 by jho               #+#    #+#             */
-/*   Updated: 2024/01/26 18:01:37 by sejkim2          ###   ########.fr       */
+/*   Updated: 2024/01/27 11:55:41 by sejkim2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ t_mrt	*mrt_init(int argc, char **argv)
 	if (mrt == NULL)
 		ft_except("Malloc failed.", errno);
 	mrt->objs = NULL;
-	mrt_file_parse(mrt, argc, argv);
+	if (mrt_file_parse(mrt, argc, argv) == false)
+		return (NULL);
 	/* Not implemented. */
 	/*
 	 *	This section is for test only.
