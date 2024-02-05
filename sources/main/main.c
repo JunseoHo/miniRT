1ea48288f737a050bf65e495c27ef48e8d8ccaa5
Sejin Kim <sejkim2@c2r10s1.42seoul.kr>
2024-02-05 20:23:10 +0900

    add : shadow but fail...

--- sources/main/main.c
+++ sources/main/main.c
@@ -6,13 +6,13 @@
 /*   By: sejkim2 <sejkim2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 14:40:19 by jho               #+#    #+#             */
-/*   Updated: 2024/02/03 15:18:10 by sejkim2          ###   ########.fr       */
+/*   Updated: 2024/02/05 17:45:19 by sejkim2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/mrt.h"
 
-static	void	parse_test_print(t_mrt *mrt)
+void	parse_test_print(t_mrt *mrt)
 {
 	t_obj *objs = mrt->objs;
 	t_lit *lits = mrt->lits;
@@ -48,11 +48,9 @@ int	main(int argc, char **argv)
 	t_mrt	*mrt;
 	t_scene	*scene;
 
-	atexit(leaks);
 	mrt = mrt_init(argc, argv);
 	if (mrt == NULL)
 		ft_except("Error", 1);
-	parse_test_print(mrt);
 	scene = scene_init("miniRT");
 	mrt_raytrace(mrt, scene);
 	scene_show(scene);
--- sources/mrt/mrt_raycast.c
+++ sources/mrt/mrt_raycast.c
@@ -6,26 +6,62 @@
 /*   By: sejkim2 <sejkim2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 03:30:32 by jho               #+#    #+#             */
-/*   Updated: 2024/02/02 13:57:27 by sejkim2          ###   ########.fr       */
+/*   Updated: 2024/02/05 20:22:52 by sejkim2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/mrt.h"
+
+// t_vec	mrt_raycast(t_mrt *mrt, t_ray ray)
+// {
+// 	t_obj	*objs;
+// 	t_hit	hit;
+// 	t_hit	hit_nearest;
+// 	t_vec	light_dir;
+// 	t_ray	light_ray;
+
+// 	objs = mrt->objs;
+// 	hit_nearest.dist = DIST_MAX;
+// 	while (objs != NULL)
+// 	{
+// 		if (mrt_hit(objs, ray, &hit) && hit.dist < hit_nearest.dist)
+// 			hit_nearest = hit;
+// 		objs = objs->next;
+// 	}
+// 	light_dir = vec_norm(vec_sub(mrt->lits->origin, hit.origin));
+// 	light_ray = mrt_ray(hit.normal, light_dir);
+// 	if (mrt_hit(mrt->objs, light_ray, &hit_nearest) == TRUE)
+// 		return (mrt_color(0, 0, 0));
+// 	if (hit_nearest.dist != DIST_MAX)
+// 		return (mrt_phong(mrt, ray, &hit_nearest));
+// 	return (mrt_color(0, 0, 0));
+// }
 
 t_vec	mrt_raycast(t_mrt *mrt, t_ray ray)
 {
 	t_obj	*objs;
 	t_hit	hit;
 	t_hit	hit_nearest;
+	t_vec	light_dir;
+	t_ray	light_ray;
+	t_obj	*hit_obj;
 
 	objs = mrt->objs;
+	hit_obj = objs;
 	hit_nearest.dist = DIST_MAX;
 	while (objs != NULL)
 	{
 		if (mrt_hit(objs, ray, &hit) && hit.dist < hit_nearest.dist)
+		{
 			hit_nearest = hit;
+			hit_obj = objs;
+		}
 		objs = objs->next;
 	}
+	light_dir = vec_norm(vec_sub(mrt->lits->origin, hit.origin));
+	light_ray = mrt_ray(hit.normal, light_dir);
+	if (hit_obj != NULL && mrt_hit(hit_obj, light_ray, &hit_nearest) == TRUE)
+		return (mrt_color(0, 0, 0));
 	if (hit_nearest.dist != DIST_MAX)
 		return (mrt_phong(mrt, ray, &hit_nearest));
 	return (mrt_color(0, 0, 0));
