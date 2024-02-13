8c9a5004a381aeeb53f4d4cccc52faf369639edd
Sejin Kim <sejkim2@c2r10s8.42seoul.kr>
2024-02-13 20:39:20 +0900

    fix : 광원 하나일 경우에만,  hard shadow

--- sources/mrt/mrt_raycast.c
+++ sources/mrt/mrt_raycast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mrt_raycast.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jho <jho@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: sejkim2 <sejkim2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 03:30:32 by jho               #+#    #+#             */
-/*   Updated: 2024/02/12 16:26:03 by jho              ###   ########.fr       */
+/*   Updated: 2024/02/13 20:38:01 by sejkim2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ t_bool	mrt_hard_shadow(t_obj *objs, t_lit lit, t_hit *hit)
 	t_vec	light_dir;
 	t_ray	light_ray;
 	t_hit	dummy_hit;
-	t_bool	b_shadowed;
-	
-	b_shadowed = FALSE;
+
 	objs_cpy = objs;
 	light_dir = vec_norm(vec_sub(lit.origin, hit->origin));
 	light_ray = mrt_ray(hit->origin, light_dir);
@@ -29,15 +27,10 @@ t_bool	mrt_hard_shadow(t_obj *objs, t_lit lit, t_hit *hit)
 	while (objs_cpy != NULL)
 	{
 		if (mrt_hit(objs_cpy, light_ray, &dummy_hit) == TRUE)
-		{
-			b_shadowed = TRUE;
-			break ;
-		}
+			return (TRUE);
 		objs_cpy = objs_cpy->next;
 	}
-	if (b_shadowed == FALSE)
-		return (FALSE);
-	return (TRUE);
+	return (FALSE);
 }
 
 t_vec	mrt_raycast(t_mrt *mrt, t_ray ray)
@@ -54,8 +47,12 @@ t_vec	mrt_raycast(t_mrt *mrt, t_ray ray)
 			hit_nearest = hit;
 		objs = objs->next;
 	}
+	t_vec color;
+	color = vec(0, 0, 0);
+	color = vec_add(color, vec_scale(mrt->amb.color, mrt->amb.ratio));
 	if (hit_nearest.dist == DIST_MAX
 		|| mrt_hard_shadow(mrt->objs, mrt->lit, &hit_nearest))
-		return (mrt_color(0, 0, 0));
+		return (vec(hit_nearest.albedo.x * mrt->amb.ratio, hit_nearest.albedo.y * mrt->amb.ratio, hit_nearest.albedo.z * mrt->amb.ratio));
+		// return (mrt_color(0, 0, 0));
 	return (mrt_phong(mrt, ray, &hit_nearest));
 }
